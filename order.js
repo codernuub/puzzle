834bedb57938be0a5a71dd73f0841bd8c1d977de
2xcell.offline <sardarbhai6@gmail.com>
2025-04-18 15:07:49 +0530

    Added buttons

---
+++ order.js
@@ -0,0 +1,157 @@
+const centerOrder = [
+  {
+    order: 1,
+    state: "ladakh",
+    distance: 0,
+  },
+  {
+    order: 2,
+    state: "jammu",
+    distance: 30.141102599392262,
+  },
+  {
+    order: 3,
+    state: "himachal",
+    distance: 61.27060266943665,
+  },
+  {
+    order: 4,
+    state: "punjab",
+    distance: 75.90353271585697,
+  },
+  {
+    order: 5,
+    state: "uttarakhand",
+    distance: 106.48930576309469,
+  },
+  {
+    order: 6,
+    state: "haryana",
+    distance: 109.50939241304944,
+  },
+  {
+    order: 7,
+    state: "delhi",
+    distance: 123.95908359906923,
+  },
+  {
+    order: 8,
+    state: "rajasthan",
+    distance: 168.13503718872508,
+  },
+  {
+    order: 9,
+    state: "uttarpradesh",
+    distance: 168.9660264106639,
+  },
+  {
+    order: 10,
+    state: "madhya",
+    distance: 216.89130426008646,
+  },
+  {
+    order: 11,
+    state: "bihar",
+    distance: 240.01402163135836,
+  },
+  {
+    order: 12,
+    state: "sikkim",
+    distance: 251.8634634239195,
+  },
+  {
+    order: 13,
+    state: "gujarat",
+    distance: 259.7667654010623,
+  },
+  {
+    order: 14,
+    state: "jharkhand",
+    distance: 271.1117555660606,
+  },
+  {
+    order: 15,
+    state: "westbengal",
+    distance: 283.2197840955782,
+  },
+  {
+    order: 16,
+    state: "chattishgarh",
+    distance: 291.7989229908903,
+  },
+  {
+    order: 17,
+    state: "maharastra",
+    distance: 313.014747062456,
+  },
+  {
+    order: 18,
+    state: "meghalaya",
+    distance: 314.21525565639905,
+  },
+  {
+    order: 19,
+    state: "odisha",
+    distance: 318.7517728536215,
+  },
+  {
+    order: 20,
+    state: "assam",
+    distance: 330.9635437492599,
+  },
+  {
+    order: 21,
+    state: "arunanchal",
+    distance: 335.8338562628873,
+  },
+  {
+    order: 22,
+    state: "telagana",
+    distance: 336.04823236565124,
+  },
+  {
+    order: 23,
+    state: "tripura",
+    distance: 342.7850231866716,
+  },
+  {
+    order: 24,
+    state: "nagaland",
+    distance: 353.0896687556474,
+  },
+  {
+    order: 25,
+    state: "manipur",
+    distance: 360.7935438199534,
+  },
+  {
+    order: 26,
+    state: "mizoram",
+    distance: 363.5624649898377,
+  },
+  {
+    order: 27,
+    state: "andhra",
+    distance: 376.8159560919396,
+  },
+  {
+    order: 28,
+    state: "goa",
+    distance: 383.78814266469817,
+  },
+  {
+    order: 29,
+    state: "karnataka",
+    distance: 386.6563526582239,
+  },
+  {
+    order: 30,
+    state: "tamil",
+    distance: 464.8274332037193,
+  },
+  {
+    order: 31,
+    state: "kerala",
+    distance: 469.3704511208063,
+  },
+];
---
+++ tests/orders/order1.js
@@ -0,0 +1,162 @@
+const order1 = [
+  {
+    order: 0,
+    state: "ladakh",
+    distance: 0,
+  },
+  {
+    order: 1,
+    state: "ladakh",
+    distance: 0,
+  },
+  {
+    order: 2,
+    state: "jammu",
+    distance: 393.4085195151882,
+  },
+  {
+    order: 3,
+    state: "himachal",
+    distance: 850.9292009769126,
+  },
+  {
+    order: 4,
+    state: "punjab",
+    distance: 943.658616809581,
+  },
+  {
+    order: 5,
+    state: "haryana",
+    distance: 1306.8274490945175,
+  },
+  {
+    order: 6,
+    state: "uttarakhand",
+    distance: 1370.9057427836972,
+  },
+  {
+    order: 7,
+    state: "rajasthan",
+    distance: 1415.6607172874574,
+  },
+  {
+    order: 8,
+    state: "uttarpradesh",
+    distance: 1451.7844570296047,
+  },
+  {
+    order: 9,
+    state: "delhi",
+    distance: 1713.0505264615983,
+  },
+  {
+    order: 10,
+    state: "madhya",
+    distance: 2150.251056243829,
+  },
+  {
+    order: 11,
+    state: "bihar",
+    distance: 2519.0651007643137,
+  },
+  {
+    order: 12,
+    state: "gujarat",
+    distance: 2532.7392232948578,
+  },
+  {
+    order: 13,
+    state: "sikkim",
+    distance: 2978.379540633031,
+  },
+  {
+    order: 14,
+    state: "maharastra",
+    distance: 3005.226902831168,
+  },
+  {
+    order: 15,
+    state: "chattishgarh",
+    distance: 3006.8594707623947,
+  },
+  {
+    order: 16,
+    state: "westbengal",
+    distance: 3088.783342675483,
+  },
+  {
+    order: 17,
+    state: "jharkhand",
+    distance: 3233.5058249252397,
+  },
+  {
+    order: 18,
+    state: "telagana",
+    distance: 3480.9641510569227,
+  },
+  {
+    order: 19,
+    state: "odisha",
+    distance: 3495.6843804130585,
+  },
+  {
+    order: 20,
+    state: "meghalaya",
+    distance: 3715.5402218775803,
+  },
+  {
+    order: 21,
+    state: "karnataka",
+    distance: 3724.591637756519,
+  },
+  {
+    order: 22,
+    state: "tripura",
+    distance: 3917.6746528313683,
+  },
+  {
+    order: 23,
+    state: "arunanchal",
+    distance: 3944.9511883754594,
+  },
+  {
+    order: 24,
+    state: "andhra",
+    distance: 3961.1003122510638,
+  },
+  {
+    order: 25,
+    state: "mizoram",
+    distance: 3989.1282621988316,
+  },
+  {
+    order: 26,
+    state: "nagaland",
+    distance: 4035.536462458488,
+  },
+  {
+    order: 27,
+    state: "manipur",
+    distance: 4049.6702655979493,
+  },
+  {
+    order: 28,
+    state: "assam",
+    distance: 4054.428656026889,
+  },
+  {
+    order: 29,
+    state: "goa",
+    distance: 4200.79357732378,
+  },
+  {
+    order: 30,
+    state: "tamil",
+    distance: 4707.644912061464,
+  },
+  {
+    order: 31,
+    state: "kerala",
+    distance: 4749.363961387402,
+  },
+];
