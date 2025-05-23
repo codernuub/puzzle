bf8df9f2eea9d5726621aacc07ee6372624d434e
2xcell.offline <sardarbhai6@gmail.com>
2025-05-23 20:22:40 +0530

    Added main page template

---
+++ js/data.js
@@ -0,0 +1,15 @@
+const data = {
+  heading: "Map Of India Activity",
+  activities: [
+    {
+      id: 1,
+      thumbnail: "thumb1.png",
+      name: "State Map",
+    },
+    {
+      id: 2,
+      thumbnail: "thumb2.png",
+      name: "Map Puzzle",
+    }
+  ],
+};
