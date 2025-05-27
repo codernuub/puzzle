08f9dfa58363a674d07c81a666622e477ff5968c
2xcell.offline <sardarbhai6@gmail.com>
2025-05-27 12:30:21 +0530

    Resolved activity 1

--- 1/js/data/activity.js
+++ 1/js/data/activity.js
@@ -1,5 +1,5 @@
 const ACTIVITY = {
   instruction: "Drag and Drop the state name on correct place",
-  states: ["Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh"], //If empty it will generate random states
+  states: [], //If empty it will generate random states
   randomStatesCount: 10, //By default generate 10 random states
 };
